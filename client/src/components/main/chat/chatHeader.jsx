297e61cc75b8ab3522b88fb1a7acfd8c7415e8cc
Sahil Singh <sahil.code3@gmail.com>
2023-12-13 14:37:42 +0530

    feat: Implement main MessagesHeader ui skeleton

--- client/src/components/main/chat/chatHeader.jsx
+++ client/src/components/main/chat/chatHeader.jsx
@@ -1,7 +1,8 @@
 import MobileToggle from "@/components/mobileToggle";
+import { Separator } from "@/components/ui/separator";
 import { UserAvatar } from "@/components/userAvatar";
 import useServer from "@/hooks/useServer";
-import { Hash } from "lucide-react";
+import { Hash, Users } from "lucide-react";
 import React from "react";
 
 const ChatHeader = ({ type, data }) => {
@@ -12,10 +13,28 @@ const ChatHeader = ({ type, data }) => {
   // else Get the name from data
 
   return (
-    <div className="text-md font-semibold px-3 flex items-center h-10 border-neutral-200 dark:border-neutral-800 border-b-2">
+    <div className="pl-4 text-md px-3 flex items-center h-10 border-neutral-200 dark:border-neutral-800 border-b-2">
       <MobileToggle />
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+      {(type === "messages" || type === "conversation") && (
+        <div className="group flex items-center gap-x-2 ">
+          <div className="flex items-center gap-x-2">
+            <Users className="h-5 w-5 text-zinc-400" />
+            <p>Friends</p>
+          </div>
+          <Separator className="mt-[1px] h-5 w-[1px] bg-zinc-300 dark:bg-zinc-700 rounded-md m-1" />
+          <p className="hover:bg-zinc-700/10 dark:hover:bg-zinc-600/50 dark:hover:text-zinc-200 transition px-2 rounded-sm text-zinc-400">
+            Online
+          </p>
+          <p className="hover:bg-zinc-700/10 dark:hover:bg-zinc-600/50 dark:hover:text-zinc-200 transition px-2 rounded-sm text-zinc-400">
+            All
+          </p>
+          <p className="hover:bg-zinc-700/10 dark:hover:bg-zinc-600/50 dark:hover:text-zinc-200 transition px-2 rounded-sm text-zinc-400">
+            Pending
+          </p>
+        </div>
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
     </div>
