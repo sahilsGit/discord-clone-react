c7cfc52b91f6845d419903b62cc004a93a242b3d
Sahil Singh <sahil.code3@gmail.com>
2023-12-13 14:41:33 +0530

    style: change DM button's onClick behaviour

--- client/src/components/main/channel/channelMain.jsx
+++ client/src/components/main/channel/channelMain.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ChatHeader from "../chat/chatHeader";
 import ChatInput from "../chat/chatInput";
+import useServer from "@/hooks/useServer";
 
 const ChannelMain = () => {
+  const serverDetails = useServer("serverDetails");
+  if (!serverDetails) {
+    return <p>loading.....</p>;
+  }
   return (
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
       <ChatHeader type="channel" data={null} />
