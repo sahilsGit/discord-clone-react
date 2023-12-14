882c56ccd737da87bbfefcaf1c6538288a95f764
Sahil Singh <sahil.code3@gmail.com>
2023-12-14 09:02:32 +0530

    feat: extend chatHeader UI, add skeleton buttons

--- client/src/components/main/chat/chatHeader.jsx
+++ client/src/components/main/chat/chatHeader.jsx
@@ -19,21 +19,31 @@ const ChatHeader = ({ type, data }) => {
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
       {(type === "messages" || type === "conversation") && (
-        <div className="group flex items-center gap-x-2 ">
+        <div className="group flex justify-between gap-x-2 w-full">
           <div className="flex items-center gap-x-2">
-            <Users className="h-5 w-5 text-zinc-400" />
-            <p>Friends</p>
+            <div className="flex items-center gap-x-2">
+              <Users className="h-5 w-5 text-zinc-400" />
+              <p>Friends</p>
+            </div>
+            <Separator className="mt-[1px] h-5 w-[1px] bg-zinc-300 dark:bg-zinc-700 rounded-md m-1" />
+            <p className="hover:bg-zinc-700/10 dark:hover:bg-zinc-600/50 dark:hover:text-zinc-200 transition px-2 rounded-sm text-zinc-400">
+              Online
+            </p>
+            <p className="hover:bg-zinc-700/10 dark:hover:bg-zinc-600/50 dark:hover:text-zinc-200 transition px-2 rounded-sm text-zinc-400">
+              All
+            </p>
+            <p className="hover:bg-zinc-700/10 dark:hover:bg-zinc-600/50 dark:hover:text-zinc-200 transition px-2 rounded-sm text-zinc-400">
+              Pending
+            </p>
           </div>
-          <Separator className="mt-[1px] h-5 w-[1px] bg-zinc-300 dark:bg-zinc-700 rounded-md m-1" />
-          <p className="hover:bg-zinc-700/10 dark:hover:bg-zinc-600/50 dark:hover:text-zinc-200 transition px-2 rounded-sm text-zinc-400">
-            Online
-          </p>
-          <p className="hover:bg-zinc-700/10 dark:hover:bg-zinc-600/50 dark:hover:text-zinc-200 transition px-2 rounded-sm text-zinc-400">
-            All
-          </p>
-          <p className="hover:bg-zinc-700/10 dark:hover:bg-zinc-600/50 dark:hover:text-zinc-200 transition px-2 rounded-sm text-zinc-400">
-            Pending
-          </p>
+          <div className="flex gap-x-2 items-center justify-center">
+            <p className="hover:bg-zinc-700/10 dark:hover:bg-zinc-600/50 dark:hover:text-zinc-200 transition px-2 rounded-sm text-zinc-400">
+              Blocked
+            </p>
+            <div className="bg-emerald-600 rounded-sm px-2">
+              <p>Add Friend</p>
+            </div>
+          </div>
         </div>
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
